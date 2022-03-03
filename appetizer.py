4eb7854245a521d48131c88b97239a26742535da
Joseph Casey <100725719+JosephECasey@users.noreply.github.com>
2022-03-03 05:44:43 -0600

    Add files via upload

---
+++ appetizer.py
@@ -0,0 +1,14 @@
+# Programmer: Joseph Casey
+# Date: 3/1/2022
+# Module name: appetizer
+
+# Collection of user input for price, and amount of appetizer(s)
+
+# Appetizer price:
+app_price = float(input("Enter appetizer price: "))
+
+# Appetizer amount:
+app_amount = float(input("Enter amount of appetizers purchased: "))
+
+# Calculated total price of appetizers
+app_total = app_price * app_amount
---
+++ dessert.py
@@ -0,0 +1,14 @@
+# Programmer: Joseph Casey
+# Date: 3/3/2022
+# Name: Dessert module
+
+# Collection of user input for price, and amount of dessert(s)
+
+# Dessert price:
+dsrt_price = float(input("Enter dessert price: "))
+
+# Dessert amount:
+dsrt_amount = float(input("Enter amount of desserts purchased: "))
+
+# Calculated total price of desserts
+dsrt_total = dsrt_price * dsrt_amount
